51d758020a75d31a06bbe9e394c85c18660eb071
Sarib Kashif <kashifsar01@gmail.com>
2019-12-30 20:21:48 -0500

    first commit

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour {
+
+    public float bounceScale = 10000;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject thing = other.gameObject;
+        Rigidbody rb = thing.GetComponent<Rigidbody>();
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        else if (thing.CompareTag("Enemies"))
+        {
+
+        }
+    }
+}
